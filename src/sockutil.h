dcf7ec4615e5c1deb3b2268270f16c2d665d0050
Marti Bolivar <mbolivar@leaflabs.com>
2013-03-20 15:41:29 -0400

    sockutil.h: better Doxygen
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- src/sockutil.h
+++ src/sockutil.h
@@ -11,9 +11,14 @@
 #include <stdint.h>
 
 /**
+ * @brief Convenience for creating UDP sockets.
+ *
  * Try to socket() and bind() a new UDP socket at the specified port,
- * using IPv4 or IPv6 as available. On success, returns the socket
- * file descriptor. On failure, returns a invalid socket (-1).
+ * using IPv4 or IPv6 as available.
+ *
+ * @param port Port to open a socket on.
+ * @return On success, the socket file descriptor. On failure, an
+ *         invalid socket (-1).
  */
 int sockutil_get_udp_socket(uint16_t port);
 
