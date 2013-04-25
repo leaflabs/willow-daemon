350295912484ab28860fb943ad1f39724bf7ecad
Marti Bolivar <mbolivar@leaflabs.com>
2013-04-25 12:30:11 -0400

    test: test-raw-packets: rip out outdated tests
    These test the old packet protocol, which has been replaced. We'll
    need new tests once the revised implementation is done.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- test/test-raw-packets/main.c
+++ test/test-raw-packets/main.c
@@ -38,50 +38,20 @@
 #define ck_assert_int_gt_h(X, Y) _ck_assert_int_hex(X, >, Y)
 #define ck_assert_int_ge_h(X, Y) _ck_assert_int_hex(X, >=, Y)
 
-/* Values to use for raw_packet.p.bsamp.bs_(nchips,nlines) */
-#define BSAMP_FLAGS  (RAW_FLAG_BSAMP_IS_LIVE | RAW_FLAG_BSAMP_IS_LAST)
-#define BSAMP_IDX    0xAA550011
-#define BSAMP_NCHIPS 0x0002
-#define BSAMP_NLINES 0x0100
-
-/* A pair of initialized bsamp packets with above configuration */
-struct raw_packet *rsend;
-struct raw_packet *rsend_copy;  /* raw_packet_send() trashes its argument */
-struct raw_packet *rrecv;
-
 /* A socket pair to send and receive packets. */
 int sockfd[2];
 
 static void setup_raw(void)
 {
-    rsend = raw_packet_create_bsamp(BSAMP_NCHIPS, BSAMP_NLINES);
-    rrecv = raw_packet_create_bsamp(BSAMP_NCHIPS, BSAMP_NLINES);
-    rsend_copy = raw_packet_create_bsamp(BSAMP_NCHIPS, BSAMP_NLINES);
-
-    if (rsend == 0 || rrecv == 0 || rsend_copy == 0 ||
-        socketpair(AF_UNIX, SOCK_DGRAM, 0, sockfd) != 0) {
-        /* For sanity in writing test cases, they'll all be there, or
-         * none will. */
-        rsend = rrecv = rsend_copy = 0;
+    if (socketpair(AF_UNIX, SOCK_DGRAM, 0, sockfd) != 0) {
         sockfd[0] = -1;
         sockfd[1] = -1;
         return;
     }
-
-    rsend->p_flags = BSAMP_FLAGS;
-    rsend->p.bsamp.bs_idx = BSAMP_IDX;
-    for (size_t i = 0; i < raw_packet_nsamps(rsend); i++) {
-        rsend->p.bsamp.bs_samples[i] = (raw_samp_t)i;
-    }
-
-    raw_packet_copy(rsend_copy, rsend);
 }
 
 static void teardown_raw(void)
 {
-    free(rsend);
-    free(rsend_copy);
-    free(rrecv);
     if (sockfd[0] != -1) {
         close(sockfd[0]);
     }
@@ -99,77 +69,29 @@ START_TEST(test_sizes_packing)
      * Checking this here instead of forcing __packed is a performance
      * optimization -- we don't want to force the compiler to pack, or
      * dealing with raw data will generate slow code. */
-    ck_assert_int_eq(sizeof(struct raw_msg_bsamp), 8);
-    ck_assert_int_eq(offsetof(struct raw_msg_bsamp, bs_idx), 0);
-    ck_assert_int_eq(offsetof(struct raw_msg_bsamp, bs_nchips), 4);
-    ck_assert_int_eq(offsetof(struct raw_msg_bsamp, bs_nlines), 6);
-    ck_assert_int_eq(offsetof(struct raw_msg_bsamp, bs_samples), 8);
-    ck_assert_int_eq(sizeof(struct raw_msg_req), 8);
-    ck_assert_int_eq(offsetof(struct raw_msg_req, r_id), 0);
-    ck_assert_int_eq(offsetof(struct raw_msg_req, r_type), 2);
-    ck_assert_int_eq(offsetof(struct raw_msg_req, r_addr), 3);
-    ck_assert_int_eq(offsetof(struct raw_msg_req, r_val), 4);
-    ck_assert_int_eq(sizeof(struct raw_msg_res), 8);
-    ck_assert_int_eq(sizeof(struct raw_packet), 12);
-    ck_assert_int_eq(offsetof(struct raw_packet, _p_magic), 0);
-    ck_assert_int_eq(offsetof(struct raw_packet, _p_proto_vers), 1);
-    ck_assert_int_eq(offsetof(struct raw_packet, p_type), 2);
-    ck_assert_int_eq(offsetof(struct raw_packet, p_flags), 3);
+    /* TODO */
 }
 END_TEST
 
 START_TEST(test_create_bsamp)
 {
     /* Basic sanity checks for raw_packet_create_bsamp(). */
-    ck_assert(rsend != 0);
-    ck_assert_int_eq(raw_packet_nsamps(rsend), BSAMP_NCHIPS * BSAMP_NLINES);
-    ck_assert_int_eq(raw_packet_sampsize(rsend),
-                     BSAMP_NCHIPS * BSAMP_NLINES * sizeof(raw_samp_t));
-    ck_assert_int_eq(rsend->p_type, RAW_PKT_TYPE_BSAMP);
+    /* TODO */
 }
 END_TEST
 
 START_TEST(test_roundtrips)
 {
-    size_t i;
-
-    ck_assert(rsend != 0 && rrecv != 0 && rsend_copy != 0);
-
-    struct raw_msg_bsamp *smsg = &rsend->p.bsamp,
-        *rmsg = &rrecv->p.bsamp,
-        *scmsg = &rsend_copy->p.bsamp;
-
     /* Round-trip rsend into rrecv through socket pair. */
-    ck_assert(socketpair(AF_UNIX, SOCK_DGRAM, 0, sockfd) == 0);
-    ssize_t send_status = raw_packet_send(sockfd[0], rsend, 0);
-    ck_assert_int_gt(send_status, 0);
-    ck_assert_int_eq(rrecv->p_type, RAW_PKT_TYPE_BSAMP);
-    ssize_t recv_status = raw_packet_recv(sockfd[1], rrecv, 0);
-    ck_assert_int_gt(recv_status, 0);
+    /* TODO */
 
     /* White box: check rsend's multibyte fields got swapped properly. */
-    ck_assert_int_eq_h(smsg->bs_idx, htonl(BSAMP_IDX));
-    ck_assert_int_eq_h(smsg->bs_nchips, htons(BSAMP_NCHIPS));
-    ck_assert_int_eq_h(smsg->bs_nlines, htons(BSAMP_NLINES));
-    for (i = 0; i < raw_bsamp_nsamps(smsg); i++) {
-        ck_assert_int_eq_h(smsg->bs_samples[i], htons(scmsg->bs_samples[i]));
-    }
+    /* TODO */
 
     /* Make sure the packet came back correctly by checking against
      * rsend_copy.  Use hex macros here; any errors are probably from
      * byte order. */
-    ck_assert_int_eq_h(rsend_copy->_p_magic, rrecv->_p_magic);
-    ck_assert_int_eq_h(rsend_copy->_p_proto_vers, rrecv->_p_proto_vers);
-    ck_assert_int_eq_h(rsend_copy->p_type, rrecv->p_type);
-    ck_assert_int_eq_h(RAW_PKT_TYPE_BSAMP, rsend_copy->p_type);
-    ck_assert_int_eq_h(rsend_copy->p_flags, rrecv->p_flags);
-    ck_assert_int_eq_h(raw_bsamp_nsamps(scmsg), raw_bsamp_nsamps(rmsg));
-    ck_assert_int_eq_h(scmsg->bs_idx, rmsg->bs_idx);
-    ck_assert_int_eq_h(scmsg->bs_nchips, rmsg->bs_nchips);
-    ck_assert_int_eq_h(scmsg->bs_nlines, rmsg->bs_nlines);
-    for (i = 0; i < raw_bsamp_nsamps(scmsg); i++) {
-        ck_assert_int_eq_h(scmsg->bs_samples[i], rmsg->bs_samples[i]);
-    }
+    /* TODO */
 }
 END_TEST
 
