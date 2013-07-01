9b2f5b3f6ac9221b4dd939bbccfdc7c31641110f
Marti Bolivar <mbolivar@leaflabs.com>
2013-07-01 17:28:37 -0400

    hdf5_ch_storage: implement cs_open() routine
    This opens an HDF5 file and creates a dataset which is modeled as an
    array of board samples of zero initial and unlimited maximum size. The
    HDF5-side model looks like this (output from h5dump --header):
    
        GROUP "/" {
           DATASET "dummy-dataset" {
              DATATYPE  H5T_COMPOUND {
                 H5T_STD_U8LE "ph_flags";
                 H5T_STD_U32LE "samp_index";
                 H5T_STD_U32LE "chip_live";
                 H5T_ARRAY { [1120] H5T_STD_U16LE } "samples";
              }
              DATASPACE  SIMPLE { ( 0 ) / ( H5S_UNLIMITED ) }
              ATTRIBUTE "board_id" {
                 DATATYPE  H5T_STD_U32LE
                 DATASPACE  SIMPLE { ( 1 ) / ( 1 ) }
              }
              ATTRIBUTE "experiment cookie" {
                 DATATYPE  H5T_STD_U64LE
                 DATASPACE  SIMPLE { ( 1 ) / ( 1 ) }
              }
              ATTRIBUTE "raw_mtype" {
                 DATATYPE  H5T_STD_U8LE
                 DATASPACE  SIMPLE { ( 1 ) / ( 1 ) }
              }
              ATTRIBUTE "raw_proto_vers" {
                 DATATYPE  H5T_STD_U8LE
                 DATASPACE  SIMPLE { ( 1 ) / ( 1 ) }
              }
           }
        }
        }
    
    Note the following differences from struct raw_pkt_bsmp:
    
    - Board ID, experiment cookie, message type, and protocol version are
      stored as a single attribute per dataset.
    
      Because of the details of how we produce the compound datatype, we
      still waste the space associated with writing down the entire board
      sample. A future patch could fix this, but it would make
      implementing hdf5_cs_write() more complicated.
    
      (Message type and protocol version are marked "raw_" to indicate
      they're implementation details which may change; they seem worth
      including for debugging.)
    
    - Packet header "magic" field doesn't appear.
    
    - The experiment cookie is a single 64-bit value.
    
    Chunk sizes (which help determine on-disk storage layout) are chosen
    arbitrarily; we needed to make a choice to support unlimited-size
    datasets. These and the chunk cache will likely benefit from
    performance tuning.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- lib/hdf5_ch_storage.c
+++ lib/hdf5_ch_storage.c
@@ -15,21 +15,32 @@
 
 /* TODO: finish implementing these */
 
+/*
+ * Note: error logging must be handled externally, using the HDF5
+ * error routines (namespace H5E*).
+ */
+
 #include "hdf5_ch_storage.h"
 
 #include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <hdf5.h>
 
 #include "logging.h"
 #include "type_attrs.h"
 #include "ch_storage.h"
+#include "raw_packets.h"
 
+#define RANK 1                  /* we store as an array of board samples */
+#define CHUNK_DIM0 50
+#define CHUNK_DIM1 1
 #define IS_LITTLE_ENDIAN (1 == *(unsigned char *)&(const int){1})
 #define HOST_H5_ORDER (IS_LITTLE_ENDIAN ? H5T_ORDER_LE : H5T_ORDER_BE)
+#define COOKIE_H5_TYPE H5T_NATIVE_UINT64
 
 static int hdf5_cs_open(struct ch_storage *chns, unsigned flags);
 static int hdf5_cs_close(struct ch_storage *chns);
@@ -45,12 +56,49 @@ static const struct ch_storage_ops hdf5_ch_storage_ops = {
     .cs_write = hdf5_cs_write,
 };
 
+/* Convert an unsigned integer (or unsigned type) to the corresponding
+ * HDF5 type ID. */
+#define TO_H5_UTYPE(x)                                  \
+    ({                                                  \
+        hid_t __h5_utype = -1;                          \
+        size_t __x_size = sizeof(x);                    \
+        if (__x_size == 1) {                            \
+            __h5_utype = H5T_NATIVE_UINT8;              \
+        } else if (__x_size == 2) {                     \
+            __h5_utype = H5T_NATIVE_UINT16;             \
+        } else if (__x_size == 4) {                     \
+            __h5_utype = H5T_NATIVE_UINT32;             \
+        } else if (__x_size == 8) {                     \
+            __h5_utype = H5T_NATIVE_UINT64;             \
+        } else {                                        \
+            assert(0);                                  \
+        }                                               \
+        __h5_utype;                                     \
+    })
+
+/* Dataset attributes and their names -- the numbers index into
+ * h5_cs_data->h5_attrs, and aren't necessarily the same as their
+ * indices in an HDF5 file. */
+#define H5_ATTR_MTYPE 0         /* mtype (==RAW_MTYPE_BSMP) */
+#define H5_ATTR_BOARD_ID 1      /* board identifier */
+#define H5_ATTR_PVERS 2         /* protocol version */
+#define H5_ATTR_COOKIE 3        /* experiment cookie */
+#define H5_ATTR_MTYPE_NAME "raw_mtype"
+#define H5_ATTR_BOARD_ID_NAME "board_id"
+#define H5_ATTR_PVERS_NAME "raw_proto_vers"
+#define H5_ATTR_COOKIE_NAME "experiment_cookie"
+#define H5_NATTRS (H5_ATTR_COOKIE + 1)
+
 struct h5_cs_data {
     const char *dset_name;      /* dataset name */
     hid_t h5_file;              /* HDF5 file type */
     hid_t h5_dspace;            /* data space */
-    hid_t h5_dtype;             /* data type */
+    hid_t h5_arrtype;           /* sample array (within bsamp) data type */
+    hid_t h5_dtype;             /* board sample data type */
     hid_t h5_dset;              /* data set */
+    hsize_t h5_chunk_dims[2];   /* data set chunk dimensions */
+    hid_t h5_attr_dspace;       /* attribute data space */
+    hid_t h5_attrs[H5_NATTRS];  /* dataset-wide attributes (see H5_ATTR_*) */
 };
 
 static inline struct h5_cs_data* h5_data(struct ch_storage *chns)
@@ -61,17 +109,41 @@ static inline struct h5_cs_data* h5_data(struct ch_storage *chns)
 
 static void h5_ch_data_init(struct h5_cs_data *data, const char *dset_name)
 {
+    /*
+     * These could be partitioned into init-only and
+     * life-of-ch_storage groups, but it doesn't seem worth the effort
+     * right now.
+     */
     data->dset_name = dset_name;
     data->h5_file = -1;
     data->h5_dspace = -1;
+    data->h5_arrtype = -1;
     data->h5_dtype = -1;
     data->h5_dset = -1;
+    /* TODO tune the chunk dimensions and the chunk cache */
+    data->h5_chunk_dims[0] = CHUNK_DIM0;
+    data->h5_chunk_dims[1] = CHUNK_DIM1;
+    data->h5_attr_dspace = -1;
+    for (size_t i = 0; i < H5_NATTRS; i++) {
+        data->h5_attrs[i] = -1;
+    }
 }
 
 static int h5_ch_data_teardown(struct h5_cs_data *data)
 {
     int ret = 0;
+    for (size_t i = 0; i < H5_NATTRS; i++) {
+        if (data->h5_attrs[i] != -1 && H5Aclose(data->h5_attrs[i])) {
+            ret = -1;
+        }
+    }
+    if (data->h5_attr_dspace >= 0 && H5Sclose(data->h5_attr_dspace) < 0) {
+        ret = -1;
+    }
     if (data->h5_dset >= 0 && H5Dclose(data->h5_dset) < 0) {
+        ret = -1;
+    }
+    if (data->h5_arrtype >= 0 && H5Tclose(data->h5_arrtype) < 0) {
         ret = -1;
     }
     if (data->h5_dtype >= 0 && H5Tclose(data->h5_dtype) < 0) {
@@ -109,67 +181,148 @@ void hdf5_ch_storage_free(struct ch_storage *chns)
     free(chns);
 }
 
-#define O_LOG_ERR(failure, chns)                                \
-    log_ERR("can't open %s: error on %s", chns->cs_path, failure);
+/* Make the data space for storing board samples */
+static hid_t hdf5_create_dspace(struct h5_cs_data *data)
+{
+    const hsize_t cur_dim = 0, max_dim = H5S_UNLIMITED;
+    data->h5_dspace = H5Screate_simple(RANK, &cur_dim, &max_dim);
+    return data->h5_dspace;
+}
+
+/* Make the data types for storing board samples */
+static hid_t hdf5_create_dtypes(struct h5_cs_data *data)
+{
+    struct raw_pkt_bsmp bs;     /* just for type conversion/sizeof */
+    hsize_t nsamps = RAW_BSMP_NSAMP;
+    data->h5_arrtype = H5Tarray_create2(TO_H5_UTYPE(bs.b_samps[0]), 1,
+                                        &nsamps);
+    if (data->h5_arrtype < 0) {
+        return -1;
+    }
+    data->h5_dtype = H5Tcreate(H5T_COMPOUND, sizeof(struct raw_pkt_bsmp));
+    hid_t dtype = data->h5_dtype;
+    if (data->h5_dtype < 0 ||
+        H5Tinsert(dtype, "ph_flags", offsetof(struct raw_pkt_header, p_flags),
+                  TO_H5_UTYPE(bs.ph.p_flags)) < 0 ||
+        H5Tinsert(dtype, "samp_index", offsetof(struct raw_pkt_bsmp, b_sidx),
+                  TO_H5_UTYPE(bs.b_sidx)) < 0 ||
+        H5Tinsert(dtype, "chip_live",
+                  offsetof(struct raw_pkt_bsmp, b_chip_live),
+                  TO_H5_UTYPE(bs.b_chip_live)) < 0 ||
+        H5Tinsert(dtype, "samples", offsetof(struct raw_pkt_bsmp, b_samps),
+                  data->h5_arrtype) < 0) {
+        return -1;
+    }
+    return data->h5_dtype;
+}
+
+/* Make the data set itself */
+static hid_t hdf5_create_dset(struct h5_cs_data *data)
+{
+    hid_t ret = -1;
+    hid_t cprops = H5Pcreate(H5P_DATASET_CREATE);
+    if (cprops < 0) {
+        return -1;
+    }
+    if (H5Pset_chunk(cprops, RANK, data->h5_chunk_dims) >= 0) {
+        ret = H5Dcreate2(data->h5_file, data->dset_name,
+                         data->h5_dtype, data->h5_dspace,
+                         H5P_DEFAULT, cprops, H5P_DEFAULT);
+    }
+    H5Pclose(cprops);
+    if (ret >= 0) {
+        data->h5_dset = ret;
+    }
+    return ret;
+}
+
+/* Add attributes for experiment-wide packet fields */
+static hid_t hdf5_create_attrs(struct h5_cs_data *data)
+{
+    struct raw_pkt_bsmp bs;
+    raw_packet_init(&bs, RAW_MTYPE_BSMP, 0);
+
+    /* The dataset is the primary data object for these attributes. */
+    hid_t dobj = data->h5_dset;
+    assert(dobj >= 0);
+
+    /* The attributes all live in the same 1x1 dataspace. */
+    const hsize_t curd = 1, maxd = 1;
+    data->h5_attr_dspace = H5Screate_simple(1, &curd, &maxd);
+    if (data->h5_attr_dspace < 0) {
+        return -1;
+    }
+
+    /* Create the attributes. */
+    data->h5_attrs[H5_ATTR_MTYPE] = H5Acreate2(dobj, H5_ATTR_MTYPE_NAME,
+                                               TO_H5_UTYPE(bs.ph.p_mtype),
+                                               data->h5_attr_dspace,
+                                               H5P_DEFAULT, H5P_DEFAULT);
+    data->h5_attrs[H5_ATTR_BOARD_ID] = H5Acreate2(dobj, H5_ATTR_BOARD_ID_NAME,
+                                                  TO_H5_UTYPE(bs.b_id),
+                                                  data->h5_attr_dspace,
+                                                  H5P_DEFAULT, H5P_DEFAULT);
+    data->h5_attrs[H5_ATTR_PVERS] = H5Acreate2(dobj, H5_ATTR_PVERS_NAME,
+                                               TO_H5_UTYPE(bs.ph.p_proto_vers),
+                                               data->h5_attr_dspace,
+                                               H5P_DEFAULT, H5P_DEFAULT);
+    data->h5_attrs[H5_ATTR_COOKIE] = H5Acreate2(dobj, H5_ATTR_COOKIE_NAME,
+                                               COOKIE_H5_TYPE,
+                                               data->h5_attr_dspace,
+                                               H5P_DEFAULT, H5P_DEFAULT);
+    for (size_t i = 0; i < H5_NATTRS; i++) {
+        /* Note that this also checks if we missed one. */
+        if (data->h5_attrs[i] < 0) {
+            return -1;
+        }
+    }
+
+    /* Write and close any attributes with known values. */
+    if (hdf5_write_close(data->h5_attrs + H5_ATTR_MTYPE,
+                         TO_H5_UTYPE(bs.ph.p_mtype),
+                         &bs.ph.p_mtype) < 0 ||
+        hdf5_write_close(data->h5_attrs + H5_ATTR_PVERS,
+                         TO_H5_UTYPE(bs.ph.p_proto_vers),
+                         &bs.ph.p_proto_vers) < 0) {
+        return -1;
+    }
+
+    return 0;
+}
+
 static int hdf5_cs_open(struct ch_storage *chns, unsigned flags)
 {
-#if 1
-    log_WARNING("XXXXXXXXX "
-                "%s skipping open of %s, flags %u (not implemented) "
-                "XXXXXXXXX",
-                __func__, chns->cs_path, flags);
-    errno = EIO;
-    return -1;
-#else  /* FIXME finish this up */
     struct h5_cs_data tmp;
-    h5_ch_data_init(&tmp, h5_data(chns)->dset_name);
+    h5_ch_data_init(&tmp, h5_data(chns)->dset_name); /* initialize defaults */
 
     tmp.h5_file = H5Fcreate(chns->cs_path, flags, H5P_DEFAULT, H5P_DEFAULT);
     if (tmp.h5_file < 0) {
-        O_LOG_ERR("HDF5 file", chns);
         goto fail;
     }
-
-    const hsize_t cur_dim = 0, max_dim = H5S_UNLIMITED;
-    tmp.h5_dspace = H5Screate_simple(1, &cur_dim, &max_dim);
-    if (tmp.h5_dspace < 0) {
-        O_LOG_ERR("data space", chns);
+    if (hdf5_create_dspace(&tmp) < 0) {
         goto fail;
     }
-
-    assert(sizeof(unsigned short) == 2);
-    tmp.h5_dtype = H5Tcopy(H5T_NATIVE_USHORT);
-    if (tmp.h5_dtype < 0) {
-        O_LOG_ERR("data type", chns);
+    if (hdf5_create_dtypes(&tmp) < 0) {
         goto fail;
     }
-
-    herr_t err = H5Tset_order(tmp.h5_dtype, HOST_H5_ORDER);
-    if (err < 0) {
-        O_LOG_ERR("byte order", chns);
+    if (hdf5_create_dset(&tmp) < 0) {
         goto fail;
     }
-
-    tmp.h5_dset = H5Dcreate(tmp.h5_file, tmp.dset_name, tmp.h5_dtype,
-                            tmp.h5_dspace, H5P_DEFAULT);
-    if (tmp.h5_dset < 0) {
-        O_LOG_ERR("data set", chns);
+    if (hdf5_create_attrs(&tmp) < 0) {
         goto fail;
     }
-
-    /* Success! */
-    memcpy(h5_data(chns), &tmp, sizeof(tmp));
+    memcpy(h5_data(chns), &tmp, sizeof(tmp)); /* Success! */
     return 0;
+
  fail:
-    /* Free any resources acquired in tmp. */
     if (h5_ch_data_teardown(&tmp) == -1) {
         log_ERR("HDF5 teardown failed");
     }
-    /* TODO: if H5Fcreate created a file, should we notice and unlink here? */
+    if (H5Fis_hdf5(chns->cs_path) > 0 && unlink(chns->cs_path) == -1) {
+        log_ERR("can't unlink %s: %m", chns->cs_path);
+    }
     return -1;
-#endif
 }
-#undef O_LOG_ERR
 
 static int hdf5_cs_close(struct ch_storage *chns)
 {
@@ -186,6 +339,8 @@ static int hdf5_cs_write(struct ch_storage *chns,
                          size_t nsamps)
 {
     /* TODO */
+    /* FIXME: on first attribute write,
+     *     7. Write the attribute data (optional) */
     log_WARNING("XXXXXXXXX "
                 "%s skipping write of %zu board samples"
                 "from %p to %s (not implemented) "
