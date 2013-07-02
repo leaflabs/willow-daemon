225b058190af95c2d8100492fc7834de0a706901
Marti Bolivar <mbolivar@leaflabs.com>
2013-07-02 19:18:24 -0400

    raw_packets.h: delete "this is just a sketch" comment
    Assumptions about packet formats are now sufficiently hard-coded that
    we need to take this seriously indeed ;).
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- lib/raw_packets.h
+++ lib/raw_packets.h
@@ -13,8 +13,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* XXX this is just a sketch; don't take it too seriously. [mbolivar] */
-
 /**
  * @file raw_data_packets.h
  *
