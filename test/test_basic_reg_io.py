da58001dc90d0cb5a75f864e7406eef08aa2fff6
Marti Bolivar <mbolivar@leaflabs.com>
2013-07-29 15:04:10 -0400

    Add test/test_basic_reg_io.py
    Tests basic protobuf register I/O. This caught the bug fixed in ee01a9b.
    
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

---
+++ test/test_basic_reg_io.py
@@ -0,0 +1,48 @@
+# Test basic register I/O with the dummy data node
+
+import random
+import time
+import unittest
+
+import test_helpers
+from pbuf_reg_io import *
+
+# FIXME: allow the environment to show logging output to stderr, for
+# debugging tests
+
+class TestBasicRegIO(unittest.TestCase):
+
+    def setUp(self):
+        dn = open('/dev/null', 'rw+')
+        self.dn = dn
+        self.sub_kwargs = { 'stdin': dn, 'stdout': dn, 'stderr': dn }
+        self.daemon = test_helpers.daemon_sub(**self.sub_kwargs)
+        self.dnode = test_helpers.dummy_dnode_sub(**self.sub_kwargs)
+
+    def tearDown(self):
+        self.daemon.terminate()
+        self.dnode.terminate()
+        self.dn.close()
+
+    def testCentralState(self):
+        cmd = reg_read(MOD_CENTRAL, CENTRAL_STATE)
+        resps = do_reg_ios([cmd], retry=True)
+        self.assertIsNotNone(resps)
+        rsp = resps[0].reg_io
+        self.assertEqual(rsp.module, MOD_CENTRAL)
+        self.assertEqual(rsp.central, CENTRAL_STATE)
+
+    def testBasicRegIO(self):
+        cookie_h = 0xaaaa5555
+        cookie_l = 0xbbbbeeee
+        cmds = [reg_write(MOD_CENTRAL, CENTRAL_COOKIE_H, cookie_h),
+                reg_write(MOD_CENTRAL, CENTRAL_COOKIE_L, cookie_l)]
+        responses = do_reg_ios(cmds, retry=True)
+        self.assertIsNotNone(responses)
+        rsp_h, rsp_l = [r.reg_io for r in responses]
+        self.assertEqual(rsp_h.module, MOD_CENTRAL)
+        self.assertEqual(rsp_l.module, MOD_CENTRAL)
+        self.assertEqual(rsp_h.central, CENTRAL_COOKIE_H)
+        self.assertEqual(rsp_l.central, CENTRAL_COOKIE_L)
+        self.assertEqual(rsp_h.val, cookie_h)
+        self.assertEqual(rsp_l.val, cookie_l)
