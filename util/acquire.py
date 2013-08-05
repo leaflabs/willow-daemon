8ea65aa3797dfc427ac2decf7987c02c265d9642
Marti Bolivar <mbolivar@leaflabs.com>
2013-08-05 13:19:49 -0400

    util/acquire.py: refactor to use argparse
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- util/acquire.py
+++ util/acquire.py
@@ -1,56 +1,85 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
+import argparse
 import os.path
 import sys
 from time import time
 
 from daemon_control import *
 
-def usage(exit=True):
-    print("usage: acquire.py <start|stop|save_stream file nsamples>",
-          file=sys.stderr)
-    if exit:
-        sys.exit(1)
+##
+## Command handling
+##
 
-if len(sys.argv) < 2:
-    usage()
-
-# Parse arguments.
-arg = sys.argv[1]
-if arg == 'start' or arg == 'stop':
-    if len(sys.argv) != 2:
-        print("'%s' doesn't take arguments" % arg, file=sys.stderr)
-        usage()
-    enable = True if arg == 'start' else False
-elif arg == 'save_stream':
-    if len(sys.argv) != 4:
-        print('Wrong arguments to save_stream, expected: file nsamples',
-              file=sys.stderr)
-        usage()
-    try:
-        fpath = os.path.abspath(sys.argv[2])
-        nsamples = int(sys.argv[3])
-    except:
-        print('Invalid arguments to save_stream, expected: file nsamples',
-              file=sys.stderr)
-        usage()
-else:
-    usage()
-
-# Build the command.
-if arg == 'start' or arg == 'stop':
+def acquire(enable):
     cmd = ControlCommand(type=ControlCommand.ACQUIRE)
-    cmd.acquire.exp_cookie = long(time())
+    if enable:
+        cmd.acquire.exp_cookie = long(time())
     cmd.acquire.enable = enable
-else:
-    cmd = ControlCommand(type=ControlCommand.STORE)
-    cmd.store.path = fpath
-    cmd.store.nsamples = nsamples
+    return [cmd]
 
-# Run command; print the result.
-resps = do_control_cmds([cmd])
-if resps is None:
-    print("Didn't get a response", file=sys.stderr)
+def start(args):
+    return acquire(True)
+
+def stop(args):
+    return acquire(False)
+
+def save_stream(args):
+    fpath = os.path.abspath(args.file)
+    nsamples = args.nsamples
+    cmd = ControlCommand(type=ControlCommand.STORE)
+    cmd.store.path = os.path.abspath(fpath)
+    cmd.store.nsamples = nsamples
+    return [cmd]
+
+##
+## Argument parsing
+##
+
+def no_arg_parser(cmd, description):
+    return argparse.ArgumentParser(prog=cmd, description=description)
+
+save_stream_parser = argparse.ArgumentParser(
+    prog='save_stream',
+    description='Save live streaming data to disk')
+save_stream_parser.add_argument('file',
+                                help='File to store samples in')
+save_stream_parser.add_argument('nsamples', type=int,
+                                help='Number of samples to try storing')
+
+COMMAND_HANDLING = {
+    'start': (start, no_arg_parser('start', 'Start acquiring to disk')),
+    'stop': (stop, no_arg_parser('stop', 'Stop acquiring to disk')),
+    'save_stream': (save_stream, save_stream_parser)
+}
+
+##
+## main()
+##
+
+def usage():
+    print('usage: acquire.py command [[-h] command_args ...]\n'
+          'Commands:\n'
+          '%s' % ''.join(['\t%s: %s\n' % (s, h_p[1].description)
+                          for s, h_p in COMMAND_HANDLING.iteritems()]),
+          end='', file=sys.stderr)
     sys.exit(1)
-print(resps[0])
+
+def main():
+    if len(sys.argv) < 2:
+        usage()
+    cmd, cmd_args = sys.argv[1], sys.argv[2:]
+    if cmd not in COMMAND_HANDLING:
+        usage()
+    handler, parser = COMMAND_HANDLING[cmd]
+    cmds = handler(parser.parse_args(cmd_args))
+    resps = do_control_cmds(cmds)
+    if resps is None:
+        print("Didn't get a response", file=sys.stderr)
+        sys.exit(1)
+    for resp in resps:
+        print(resp, end='')
+
+if __name__ == '__main__':
+    main()
