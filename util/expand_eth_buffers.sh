eb3bb9dc4f4fbf0857b3534386c7f620644ee576
Marti Bolivar <mbolivar@leaflabs.com>
2013-08-19 14:35:14 -0400

    expand_eth_buffers.sh: take interface on command line
    Signed-off-by: Marti Bolivar <mbolivar@leaflabs.com>

--- util/expand_eth_buffers.sh
+++ util/expand_eth_buffers.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+iface=$1
+
+if [ "xx" = "x${iface}x" ] ; then
+    iface=eth0
+fi
+
+echo Reconfiguring interface $iface
+
 export UDP_BUF_SIZE=16777216;
 sudo /sbin/sysctl -w net.core.rmem_max=$UDP_BUF_SIZE net.core.wmem_max=$UDP_BUF_SIZE net.core.rmem_default=$UDP_BUF_SIZE net.core.wmem_default=$UDP_BUF_SIZE
-sudo ethtool -G eth0 rx 4096
-sudo ifconfig eth0 mtu 4096
+sudo ethtool -G $iface rx 4096
+sudo ifconfig $iface mtu 4096
